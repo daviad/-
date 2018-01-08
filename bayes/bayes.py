35d44a2d223aba51919c95641fb1192dad7af6bb
daviad <592597406@qq.com>
2018-01-08 22:31:39 +0800

    add

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -27,6 +27,13 @@ def setOfWords2Vec(vocabList,inputSet):
 		else: print ("the word:%s is not in my vocabulary!" % word)
 	return returnVec
 
+def bagOfWords2VecMN(vacabList,inputSet):
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vacabList:
+			returnVec[vocabList.index(word)] += 1
+	return returnVec
+
 def trainNB0(trainMatrix,trainCategory):
 	numTranDocs = len(trainMatrix)
 	print "numTranDocs:%s" %numTranDocs
@@ -59,6 +66,41 @@ def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
 	if p1 > p0:
 		return 1
 	else: return 0 
+
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\w*',bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+	docList = []; classList = []; fullText = [];
+	for i in range(1,26):
+		wordList = textParse(open('email/spam/%d.txt' %i).read())
+		docList.append(wordList)
+		fullText.append(wordList)
+		classList.append(1)
+		wordList = textParse(open('email/ham/%d.txt' %i).read())
+		docList.append(wordList)
+		fullText.append(wordList)
+		classList.append(0)
+	vacabList = creatVocabList(docList)
+	trainingSet = range(50)
+	testSet = []
+	for i in range(10):
+		randIndex = int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet(randIndex))
+		del(trainingSet[randIndex])
+	trainMat = []; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vec(),vocabList,docIndex[docIndex])
+		trainClasses.append(classList[docIndex])
+	p0v,p1v,pSpam = trainNB0(array(trainMat),array(trainClasses))
+	errorCount = 0
+	for docIndex in testSet:
+		wordVector = setOfWords2Vec(vocabList,docList[docIndex])
+		if classifyNB(array(wordVector),p0v,p1V,pSpam) != classList[docIndex]:	
+			errorCount += 1
+	print 'the error rate is :', float(errorCount)/len(testSet)
 
 def testingNB():
 	listOpost,listClass = loadDataSet()
